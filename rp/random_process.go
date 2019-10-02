591c4f0d3654c958cad4d8ae73a89b44b25d7724
Changkun Ou <hi@changkun.us>
2019-10-02 10:54:57 +0200

    rp: random process for system monitoring

---
+++ rp/random_process.go
@@ -0,0 +1,172 @@
+// Copyright 2019 Changkun Ou. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+// Package rp implements a random process where identifies peak load.
+//
+// 1. Identify peak load
+// The current approach:  Mann–Whitney U test
+//
+//   Consider a count process: N1= (n_1, ..., n_{p/2}), N2 = (n_{p/2}, ..., n_{p})
+//   and our goal is to check if N2 is significant higher than N1.
+//   Let hypothesis: H0: \mu_{N_1} \leq \mu_{N_2}, and H1: \mu_{N_1} > \mu_{N_2}
+//   Then rejection region is:
+//
+//     z = \left|\frac{\mu_{N_2} - \mu_{N_1}}{\sigma_{N_1}}\right| \geq z(c)
+//
+//   We reject H0 under given confidence level.
+//   Since this approach assume the count process obey normal dist, it is
+//   recommended to have a window size higher than 15.
+//
+//
+// 2. Check if system resource can handle next peak load: Poisson + Moving average
+// The current approach: Markov Poisson process + Moving average
+//
+//   Consider p windows, and each number of events obey Poisson process,
+//   then \lambda = \frac{1}{p} \sum_{i=1}^{p} n_i, and in the next window,
+//   the probability of having k number of events is:
+//
+//     P(N=k) = \frac{\lambda^k \exp{-\lambda}}{k!}, k = 0, 1, ...
+//
+//   Assume a system can handle Q requests, then we have:
+//
+//     P(N\leq Q) = \sum_{i=0}^{Q} P(N=i) = \sum_{i=0}^{Q} \frac{\lambda^k \exp{\left(-\lambda\right)}}{k!}
+//
+// Usage in system monitoring:
+//
+//    p := rp.NewCountProcess(15, 0.05)
+//    for {
+//        time.Sleep(time.Second*30)
+//        p.Store(currentNevents) // store number of events during sleep
+//
+//        if !p.Significant() {   // 1. significant test
+//            continue
+//        }
+//        if p.Acceptable(maxNumberOfResource) { // 2. possion+moving avg test
+//            continue
+//        }
+//
+//        ... do scaling work ...
+//    }
+//
+package rp
+
+import (
+	"math"
+	"sync"
+
+	"gonum.org/v1/gonum/floats"
+	"gonum.org/v1/gonum/stat/distuv"
+)
+
+// RandomProcess defines two type of tests for given observations.
+type RandomProcess interface {
+	Store(nevent float64)
+	Significant() bool
+	Acceptable(k float64) bool
+}
+
+// CountProcess caches historical number of events,
+// which provides significant test and poission process evaluation.
+type countProcess struct {
+	maxsize    float64
+	confidence float64 // for z test and poission process
+	nevents    []float64
+
+	mu sync.RWMutex
+}
+
+// NewCountProcess creates a new counting random process.
+// maxsize represents the observation window size of the count process.
+// confidenceLevel gives the confidence level of the significant test
+// and acceptance test, it is recommended to set below 0.05.
+func NewCountProcess(maxsize, confidenceLevel float64) RandomProcess {
+	return &countProcess{
+		maxsize:    maxsize,
+		nevents:    []float64{},
+		confidence: confidenceLevel,
+	}
+}
+
+// Store stores given number of events
+func (w *countProcess) Store(nevent float64) {
+	w.mu.Lock()
+	if float64(len(w.nevents)) >= w.maxsize {
+		w.nevents = append(w.nevents[1:], nevent)
+	} else {
+		w.nevents = append(w.nevents, nevent)
+	}
+	w.mu.Unlock()
+}
+
+// Significant performs a significant greater test,
+// here we implement by z-test.
+//
+// a historical window of #events [n_1, n_2, ..., n_p],
+// we compare: [n_1, ..., n_{p/2}] and [n_{p/2}, ..., n_{p}]
+// through ztest.
+//
+// confidence coefficient is configurable
+func (w *countProcess) Significant() bool {
+	return w.ztest()
+}
+
+// Acceptable calculates whether the system can accept k events
+// on next interval, here we implement by poission process.
+//
+// Since we have tested half of the current window is significant larger
+// than previous window, therefore the probability distribution between
+// two windows are different.
+//
+// Now, we assume the recent half of window obey poission process.
+// For a historical window of #requests [n_1, n_2, ..., n_p],
+// We evaluates: [n_{p/2}, ..., n_{p}].
+func (w *countProcess) Acceptable(k float64) bool {
+	// make a copy
+	w.mu.RLock()
+	events := w.nevents[len(w.nevents)/2:]
+	w.mu.RUnlock()
+
+	// magnify avg arr accor. acceptance
+	lambda := floats.Sum(events) / (float64(len(events)))
+	acceptprob := (distuv.Poisson{Lambda: lambda}).CDF(k)
+
+	// moving average
+	future := mean(events)
+
+	return acceptprob > 1-w.confidence || future > k
+}
+
+// ztest implements a one-tailed (right) z test (assumption: Guassian).
+//
+// H0: w.events[len(w.events)/2:] <= w.events[0:len(w.events)/2]
+// H1: w.events[len(w.events)/2:] >  w.events[0:len(w.events)/2]
+// we should have w.confidence to reject H0
+func (w *countProcess) ztest() bool {
+	// copy
+	w.mu.RLock()
+	events := make([]float64, len(w.nevents))
+	copy(events, w.nevents)
+	w.mu.RUnlock()
+
+	if len(events) < 2 {
+		return false
+	}
+
+	x1 := events[len(events)/2:]    // sample
+	x2 := events[0 : len(events)/2] // population
+	return (mean(x1)-mean(x2))/stddiv(x2) > -distuv.UnitNormal.Quantile(w.confidence)
+}
+
+func mean(x []float64) float64 {
+	return floats.Sum(x) / float64(len(x))
+}
+
+func stddiv(x []float64) float64 {
+	m := mean(x)
+	var sum float64
+	for _, i := range x {
+		sum += math.Pow(float64(i)-m, 2)
+	}
+	return math.Sqrt(sum / float64(len(x)))
+}
---
+++ rp/random_process_test.go
@@ -0,0 +1,115 @@
+// Copyright 2019 Changkun Ou. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+package rp
+
+import (
+	"testing"
+)
+
+func TestRandomProcess(t *testing.T) {
+
+	t.Run("store", func(t *testing.T) {
+		counts := []float64{
+			10, 2, 3, 4, 5, 6, 6, 5, 4, 3, 100,
+			10, 2, 3, 4, 5, 6, 6, 5, 4, 3, 100,
+			10, 2, 3, 4, 5, 6, 6, 5, 4, 3, 100,
+			10, 2, 3, 4, 5, 6, 6, 5, 4, 3, 100,
+			10, 2, 3, 4, 5, 6, 6, 5, 4, 3, 100,
+			10, 2, 3, 4, 5, 6, 6, 5, 4, 3, 100,
+			10, 2, 3, 4, 5, 6, 6, 5, 4, 3, 100,
+			10, 2, 3, 4, 5, 6, 6, 5, 4, 3, 100,
+			10, 2, 3, 4, 5, 6, 6, 5, 4, 3, 100,
+			10, 2, 3, 4, 5, 6, 6, 5, 4, 3, 100,
+		}
+		window := &countProcess{maxsize: 60}
+		lc := len(counts)
+		for i := 0; i < len(counts); i++ {
+			window.Store(counts[i])
+		}
+
+		if window.maxsize != float64(len(window.nevents)) {
+			t.Fatalf("want %v, got %v", window.maxsize, float64(len(window.nevents)))
+		}
+
+		// check all elements
+		lw := len(window.nevents)
+		for i := 0; i < int(window.maxsize); i++ {
+			if window.nevents[lw-i-1] != counts[lc-i-1] {
+				t.Fatalf("want %v, got %v, index: %v", counts[lc-i-1], window.nevents[lw-i-1], i)
+			}
+		}
+	})
+
+	t.Run("Significant", func(t *testing.T) {
+		ws := map[*countProcess]bool{
+			// 100 is a significant bigger than previous one
+			&countProcess{
+				maxsize:    60,
+				nevents:    []float64{10, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 100},
+				confidence: 0.05,
+			}: true,
+			// 10 is not significant bigger enough than previous one
+			&countProcess{
+				maxsize:    60,
+				nevents:    []float64{10, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 10},
+				confidence: 0.05,
+			}: false,
+		}
+
+		for w, sig := range ws {
+			if sig != w.Significant() {
+				t.Fatalf("want %v, got %v", sig, w.Significant())
+			}
+		}
+	})
+
+	t.Run("Acceptable", func(t *testing.T) {
+		// we have 44 remaining slots for the events.
+		var remains float64 = 44
+
+		ws := map[*countProcess]bool{
+			// 44 remaining slots isn't able to handle next sample
+			// because there is a significant increasing on #events
+			&countProcess{
+				maxsize:    60,
+				nevents:    []float64{1, 2, 3, 4, 8, 16, 32, 64, 100},
+				confidence: 0.05,
+			}: false,
+			&countProcess{
+				maxsize:    60,
+				nevents:    []float64{1, 2, 4, 8, 32, 64},
+				confidence: 0.05,
+			}: false,
+			// 44 remaining slots is able to handle next sample
+			&countProcess{
+				maxsize:    60,
+				nevents:    []float64{10, 2, 3, 4, 5, 6, 6, 5, 4, 3, 2, 10},
+				confidence: 0.05,
+			}: true,
+			&countProcess{
+				maxsize:    60,
+				nevents:    []float64{0},
+				confidence: 0.05,
+			}: true,
+		}
+
+		for w, s := range ws {
+			if s != w.Acceptable(remains) {
+				t.Fatalf("want %v, got %v", s, w.Acceptable(remains))
+			}
+		}
+
+		// if emaining slots are very low, but there are (literaly)
+		// no requests at all, we think this is acceptable.
+		if !(&countProcess{
+			maxsize:    60,
+			nevents:    []float64{0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+			confidence: 0.05,
+		}).Acceptable(2) {
+			t.Fatalf("want true, got false")
+		}
+	})
+
+}
