667fd099d7836bd8827397b7da9aa4a84ae23a88
Changkun Ou <hi@changkun.us>
2020-03-23 07:55:57 +0100

    errors: try/catch/final primitives

--- errors/errors.go
+++ errors/errors.go
@@ -8,15 +8,25 @@ func New(s string) error {
 	return errors.New(s)
 }
 
-// Try catch
+// Try tries a given function and see if it throw any error
 func Try(e func() error) catch {
-	return catch{e()}
+	return catch{err: e(), result: nil}
 }
 
 type catch struct {
-	err error
+	err    error
+	result interface{}
 }
 
-func (c catch) Catch(handler func(error)) {
-	handler(c.err)
+// Catch catches the error that throwed in Try call.
+func (c catch) Catch(handler func(error) interface{}) catch {
+	if c.err != nil {
+		c.result = handler(c.err)
+	}
+	return c
+}
+
+// Final runs in any case if it is called.
+func (c catch) Final(handler func(interface{})) {
+	handler(c.result)
 }
--- errors/errors_test.go
+++ errors/errors_test.go
@@ -9,17 +9,29 @@ import (
 func TestTryCatch(t *testing.T) {
 	errors.Try(func() error {
 		return nil
-	}).Catch(func(err error) {
-		if err != nil {
-			println("on error")
-		}
+	}).Catch(func(err error) interface{} {
+		t.Fatalf("catch error: %v", err)
+		return nil
+	}).Final(func(result interface{}) {
+		t.Log("errors: everything is good")
 	})
 
 	errors.Try(func() error {
 		return errors.New("e")
-	}).Catch(func(err error) {
-		if err == nil {
-			println("no errors")
+	}).Catch(func(err error) interface{} {
+		t.Logf("capctured error: %v", err)
+		return err
+	}).Final(func(result interface{}) {
+		if result == nil {
+			t.Fatalf("cannot capture error")
+		}
+	})
+
+	errors.Try(func() error {
+		return nil
+	}).Final(func(r interface{}) {
+		if r != nil {
+			t.Fatalf("result contains error")
 		}
 	})
 }
