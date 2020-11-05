2af93df6eadbd39f24eb5328c88b9b9b8d305ae4
Changkun Ou <hi@changkun.us>
2020-11-05 21:16:23 +0100

    convert: optimize using lut
    name           old time/op  new time/op  delta
    Linear2sRGB-6  6.38µs ± 0%  0.14µs ± 0%  -97.87%  (p=0.000 n=10+8)

--- convert/color_space.go
+++ convert/color_space.go
@@ -4,10 +4,31 @@ import "math"
 
 // Linear2sRGB converts linear inputs to sRGB space.
 func Linear2sRGB(v float64) float64 {
+	i := v * lutSize
+	ifloor := int(i) & (lutSize - 1)
+	v0 := lin2sRGBLUT[ifloor]
+	v1 := lin2sRGBLUT[ifloor+1]
+	i -= float64(ifloor)
+	return v0*(1.0-i) + v1*i
+}
+
+func linear2sRGB(v float64) float64 {
 	if v <= 0.0031308 {
 		v *= 12.92
 	} else {
 		v = 1.055*math.Pow(v, 1/2.4) - 0.055
 	}
 	return v
+}
+
+const lutSize = 1024 // keep a power of 2
+
+var lin2sRGBLUT [lutSize + 1]float64
+
+func init() {
+	for i := range lin2sRGBLUT[:lutSize] {
+		lin2sRGBLUT[i] = linear2sRGB(float64(i) / lutSize)
+	}
+	// add an extra entry to avoid an "if (idxFloor == lutSize-1)"
+	lin2sRGBLUT[lutSize] = lin2sRGBLUT[lutSize-1]
 }
