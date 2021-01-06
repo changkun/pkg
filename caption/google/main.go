f48881e7360e46e0570569571d230db42605c12b
Changkun Ou <hi@changkun.de>
2021-01-06 21:51:10 +0100

    all: copyright header

--- caption/google/main.go
+++ caption/google/main.go
@@ -1,3 +1,7 @@
+// Copyright 2021 Changkun Ou. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
 // Speech to text
 // https://cloud.google.com/speech-to-text/docs/async-recognize
 // https://cloud.google.com/speech-to-text/docs/apis
